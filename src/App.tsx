324e33393022a93aa923b0b422daa1b0bd38d13b
Piriyakorn Srisook <opalpiriyakorn@gmail.com>
2026-01-13 21:38:37 +0700

    เชื่อมหน้าuserได้แระ

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
+import Users from "./pages/Users";
 import Careers from "./pages/Careers";
 import Courses from "./pages/Courses";
 import Lessons from "./pages/Lessons";
@@ -20,6 +21,7 @@ const App = () => (
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
+          <Route path="/Users" element={<Users />} />
           <Route path="/careers" element={<Careers />} />
           <Route path="/courses" element={<Courses />} />
           <Route path="/lessons" element={<Lessons />} />
