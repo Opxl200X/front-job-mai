124ca3235120297cb9e501ce0b2ff9749f436c69
Yanisa Klongkleaw <102276084+GHJulia@users.noreply.github.com>
2026-01-21 02:28:39 +0700

    210125 - Update usersData.ts

--- src/data/usersData.ts
+++ src/data/usersData.ts
@@ -1,27 +1,11 @@
-// export interface User {
-//   id: number;
-//   name: string;
-//   email: string;
-//   role: "Admin" | "User";
-//   joinedDate: string;
-//   isBanned: boolean;
-// }
-
-// export const usersData: User[] = [
-// { id: 1, name: "Tin Ny", email: "tinnapat.takananant@gmail.com", role: "Admin", joinedDate: "07/01/2569", isBanned: false },
-// { id: 2, name: "Ju Lia", email: "julia12a2005@gmail.com", role: "Admin", joinedDate: "07/01/2569", isBanned: false },
-// { id: 3, name: "O Pal", email: "opalpiriyakorn@gmail.com", role: "User", joinedDate: "09/01/2569", isBanned: true },
-// { id: 4, name: "Po Om", email: "pktsoa@gmail.com", role: "User", joinedDate: "10/01/2569", isBanned: true },
-// { id: 5, name: "Pop Eye", email: "popeye@gmail.com", role: "User", joinedDate: "11/01/2569", isBanned: true },
-// ];
-// src/data/users.ts
-
 export interface User {
   id: number;
   name: string;
   email: string;
   role: "Admin" | "User";
   joinedDate: string;
+  reports?: UserReport[];
+  banHistory?: BanHistoryEntry[];
 }
 
 export const usersData: User[] = [
@@ -31,3 +15,17 @@ export const usersData: User[] = [
   { id: 4, name: "Keitipoom Tangsrithaweesuk", email: "pktsoa@gmail.com", role: "User", joinedDate: "2025-10-12" },
   { id: 5, name: "Chanachot Bungkri", email: "popeye@gmail.com", role: "User", joinedDate: "2025-10-12" },
 ];
+
+export interface UserReport {
+  reportId: string;
+  offenderId: string;
+  lastUpdate: string;
+  reason: string;
+}
+
+export interface BanHistoryEntry {
+  banId: number;
+  banDate: string;
+  unbanDate: string;
+  reason: string;
+}
